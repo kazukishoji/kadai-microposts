3acb6cffd995c4145c63b450c892a9c86c3814bc
keykun <ka.zucca33@gmail.com>
2019-12-07 04:08:37 +0000

    kadai-microposts

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.like(micropost)
+    flash[:success] = "お気に入りを登録しました"
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unlike(micropost)
+    flash[:danger] = "お気に入り登録を解除しました"
+    redirect_back(fallback_location: root_path)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  has_many :favorites
+  has_many :favposts, through: :favorites, source: :micropost
+  
+  #フォロー機能
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -29,5 +34,20 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  #お気に入り機能
+  
+  def like(micropost)
+      favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favpost?(micropost)
+    self.favposts.include?(micropost)
   end
 end
